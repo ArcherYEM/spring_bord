bbae7fa5386aed602cc2e1b808da4e0bf5c0007a
yangem <em.yang94@gmail.com>
2023-12-12 15:24:05 +0900

    '2'

--- src/main/java/com/tjoeun/ilsan/board/free/controller/FreeBoardController.java
+++ src/main/java/com/tjoeun/ilsan/board/free/controller/FreeBoardController.java
@@ -1,8 +1,15 @@
 package com.tjoeun.ilsan.board.free.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class FreeBoardController {
+	
+	@RequestMapping(value="/board/writeView", method = RequestMethod.GET)
+	public String writeView() {
+		return "board/free/writeView";
+	}
 
 }
